fc42daca3ef985831dda7b774c794ce5986a82aa
Abel Franco Ccapa Ccamaque <37125673+abelfranco@users.noreply.github.com>
2023-07-05 12:27:16 -0500

    functions

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ import json
 app = FastAPI()
 
 # Se ingestan los datos y se crea un dataframe
-df = pd.read_csv("movies_clean.csv")
+df = pd.read_csv("data_set/movies_clean.csv")
 
 
-@app.get('/peliculas_idioma/{Idioma}')    # Se define el endpoint raiz en el decorador.
+#@app.get('/peliculas_idioma/{Idioma}')    # Se define el endpoint raiz en el decorador.
 # Decorador = 
 # Endpoint = 'peliculas_idioma'. # Idioma=es 
 # Se ejecuta cuando se hace una solicitud GET a la raiz de la API
@@ -27,17 +27,72 @@ def peliculas_idioma(Idioma: str):
     cantidad_peliculas = len(peliculas_filtradas)
     
     # Se crea el diccionario de respuesta
-    respuesta = {
-        'cantidad_peliculas': cantidad_peliculas,
-        'idioma': Idioma
-    }
-    
+    respuesta = f"{cantidad_peliculas} cantidad de peliculas fueron estrenadas en {Idioma}"
     return respuesta
 
-if __name__ == "__main__":
+#if __name__ == "__main__":
     import uvicorn   # Biblioteca de python que ejecutará y servirá a la aplicación FastAPi
     # Recibe las solictudes HTTP entrantes y enruta a la aplicación FastApi
     uvicorn.run(app, host="0.0.0.0", port=8000)
 
-
+idiomas = "English"
+resultado1 = peliculas_idioma(idiomas)
+print(resultado1)
 # http://localhost:8000/?Idioma=English
+
+def peliculas_duracion(Pelicula: str):
+    pelicula_encontrada = df[df['title'] == Pelicula]
+
+    if not pelicula_encontrada.empty:
+        duracion = pelicula_encontrada['runtime'].values[0]
+        año = pelicula_encontrada['release_year'].values[0]
+        respuesta = f"{Pelicula}. Duración: {duracion}. Año: {año}"
+        return respuesta
+    else:
+        return f"No se encontró la película: {Pelicula}"
+
+# Ejemplo de uso
+resultado2 = peliculas_duracion("Toy Story")
+print(resultado2)
+
+
+def franquicia(Franquicia: str):
+    franquicia_encontrada = df[df['belongs_to_collection'].fillna('') == Franquicia]
+
+    if not franquicia_encontrada.empty:
+        cantidad_peliculas = len(franquicia_encontrada)
+        ganancia_total = franquicia_encontrada['revenue'].sum()
+        ganancia_promedio = round(franquicia_encontrada['revenue'].mean())
+
+        return f"La franquicia {Franquicia} posee {cantidad_peliculas} películas, una ganancia total de {ganancia_total} y una ganancia promedio de {ganancia_promedio}"
+    else:
+        return f"No se encontró la franquicia: {Franquicia}"
+
+# Ejemplo de uso
+resultado3 = franquicia("Toy Story Collection")
+print(resultado3)
+ 
+def peliculas_pais(Pais: str):
+    peliculas_producidas = df[df['production_countries'].str.contains(Pais, na=False, case=False)]
+    cantidad_peliculas = len(peliculas_producidas)
+    
+    return f"Se produjeron {cantidad_peliculas} películas en el país {Pais}"
+
+# Ejemplo de uso
+pais_input = "United States"
+resultado4 = peliculas_pais(pais_input)
+print(resultado4)
+
+
+
+def productoras_exitosas(Productora: str):
+    peliculas_productora = df[df['production_companies'].str.contains(Productora, na=False, case=False)]
+    cantidad_peliculas = len(peliculas_productora)
+    revenue_total = peliculas_productora['revenue'].sum()
+    
+    return f"La productora {Productora} ha tenido un revenue de {revenue_total} y ha realizado {cantidad_peliculas} películas"
+
+# Ejemplo de uso
+productora_input = "Warner Bros."
+resultado5 = productoras_exitosas(productora_input)
+print(resultado5)
